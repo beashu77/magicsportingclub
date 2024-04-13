5099528682fd765a70a8d63ee62d1db868dc399c
Ashwini Bhoskar <ashwinibhoskar5111@gmail.com>
2024-04-13 10:25:16 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Layout from './pages/Layout';
+import { useLocation } from 'react-router-dom';
+
 function App() {
+  const location = useLocation();
+
+  // Determine if the current location matches one of the specified routes
+  const showNavbarAndFooter =
+    location.pathname === '/' ||
+    location.pathname === '/batches' ||
+    location.pathname === '/aboutus' ||
+    location.pathname === '/contact' ||
+    location.pathname === '/summercamp';
+
   return (
     <div className="App">
-    hello
+      {showNavbarAndFooter && <Navbar />} {/* Render Navbar on specified routes */}
+      <Layout />
+      {showNavbarAndFooter && <Footer />} {/* Render Footer on specified routes */}
     </div>
   );
 }
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Home from "./Home";
+import Batches from "./Batches";
+import AboutUs from "./AboutUs";
+import Contact from "./Contact";
+import SummerCamp from "./SummerCamp";
+import { Route,Routes } from 'react-router-dom';
+const NotFound = () => <h2>Page not found</h2>;
+
+const Layout = () => {
+  return (
+    <div>
+      <Routes>
+          <Route path="" element={<Home/>}/>
+          <Route path="/batches" element={<Batches/>}/>
+          <Route path="/aboutus" element={<AboutUs/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/summercamp" element={<SummerCamp/>}/>
+          <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default Layout
